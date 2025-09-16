47269d59aa56bdfd03f0f2287ef21eb7e78857e8
Erick <erickgarcia200811@gmail.com>
2025-09-16 09:56:27 -0600

    Sistema de Carpetas

---
+++ SeccionCompras/seccion.js
@@ -0,0 +1,9 @@
+export function seccionCompras() {
+
+
+    let seccion = document.createElement("section");
+    seccion.id = "seccionCompras";
+
+    let h1 = document.createElement("h1");
+    h1.textContent = "Lista de Compras";
+}
